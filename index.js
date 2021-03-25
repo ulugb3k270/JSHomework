1e65ca6162b9729faff9a19891faaa1393256322
Ulugbek <Ulugb3k270@gmail.com>
2021-03-25 23:49:39 +0530

    JS

---
+++ index.js
@@ -0,0 +1,54 @@
+console.log('----------------------------------------Informations----------------------------------------')
+
+var start = 'Lets start registration';
+alert(start);
+
+var myName = prompt ('Your Name');
+console.log ( 'Name: ' + myName);
+
+var myAge = prompt ('Your Age');
+console.log ('Age: ' + myAge);
+
+var myEmail = prompt ('E-mail address');
+console.log ('Email: ' + myEmail);
+
+var myNumber = prompt ('Mobile Phone')
+console.log('Number: ' + myNumber)
+
+var myPassword = prompt('Password')
+console.log('Password: ' + myPassword)
+
+var Welcome = 'Welcome To Webpage You Can Continue By Clicking Ok Button Below' 
+alert(Welcome)
+
+
+console.log('------------------------------------------Answers------------------------------------------')
+
+var question1 = prompt('32+23=?');
+console.log ('Question 1:  ' + '32' + '+' + '23' + '=' + (32+23 ) + ' Your Answer is: ' + question1);
+
+// if (question1 = 55) {
+//     console.log = "correct";
+// } else {
+//     console.log = "incorrect";
+// };
+
+var question2 = prompt('77*88*88=?');
+console.log ('Question 2: ' + '77' + '*' + '88' + '*' + '99' + '=' + (77*88*99) + ' Your Answer is: ' + question2 );
+
+var question3 = prompt('100/20*2=?');
+console.log('Question 3: ' + '100' + '/' + '20' + '*' + '2' + '=' + (100/2*2) + ' Your Answer is: ' + question3 );
+
+var question4 = prompt('200+300*500/100=?');
+console.log ('Question 4: ' + '200' + '+' + '300' + '*' + '500' + '/' + '100' + '=' + (200+300*500/100) + ' Your Answer is: ' + question4 )
+
+var question5 = prompt('900+233-500*12/4=?')
+console.log ('Question 5: ' + '900' + '+' + '233' + '-' + '500' + '*' + '12' + '/' + '4' + '=' + (900+233-500*12/4) + ' Your Answer is: ' + question5)
+
+var end = 'Open the console to see answers !'
+alert(end)
+
+
+
+
+
