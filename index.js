00e998f21861eb23a0abd9b3823156b9d181395f
Ulugbek <Ulugb3k270@gmail.com>
2021-03-26 14:26:28 +0530

    JS if else

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ console.log ( 'Name: ' + myName);
 var myAge = prompt ('Your Age');
 console.log ('Age: ' + myAge);
 
-var myEmail = prompt ('E-mail address');
-console.log ('Email: ' + myEmail);
-
-var myNumber = prompt ('Mobile Phone')
-console.log('Number: ' + myNumber)
-
-var myPassword = prompt('Password')
-console.log('Password: ' + myPassword)
 
 var Welcome = 'Welcome To Webpage You Can Continue By Clicking Ok Button Below' 
 alert(Welcome)
@@ -24,26 +16,51 @@ alert(Welcome)
 
 console.log('------------------------------------------Answers------------------------------------------')
 
+var correct = '+1 Ball'
+var incorrect = '-1 Ball'
+
 var question1 = prompt('32+23=?');
-console.log ('Question 1: ' + '32' + '+' + '23' + '=' + (32+23 ) + ' Your Answer is: ' + question1);
+console.log ('Question 1: ' + '32' + '+' + '23' + '=' + (32+23 ) + ' Your Answer is: ' + question1 );
 
-// if (question1 = 55) {
-//     console.log = "correct";
-// } else {
-//     console.log = "incorrect";
-// };
 
-var question2 = prompt('77*88*88=?');
-console.log ('Question 2: ' + '77' + '*' + '88' + '*' + '99' + '=' + (77*88*99) + ' Your Answer is: ' + question2 );
+if (question1 <= 55 && question1 >= 55 )
+    console.log('Your Answer Correct ✓ ' + correct);
+else 
+    console.log('Your Answer Incorrect ✘ ' + incorrect);
+
+
+
+var question2 = prompt('11*5*2=?');
+console.log ('Question 2: ' + '11' + '*' + '5' + '*' + '2' + '=' + (11*5*2) + ' Your Answer is: ' + question2 );
+
+if (question2 <= 110 && question2 >= 110 )
+    console.log('Your Answer Correct ✓ ' + correct);
+else 
+    console.log('Your Answer Incorrect ✘ ' + incorrect);
 
 var question3 = prompt('100/20*2=?');
 console.log('Question 3: ' + '100' + '/' + '20' + '*' + '2' + '=' + (100/2*2) + ' Your Answer is: ' + question3 );
 
-var question4 = prompt('200+300*500/100=?');
-console.log ('Question 4: ' + '200' + '+' + '300' + '*' + '500' + '/' + '100' + '=' + (200+300*500/100) + ' Your Answer is: ' + question4 )
+if (question3 <= 10 && question3 >= 10 )
+    console.log('Your Answer Correct ✓ ' + correct);
+else 
+    console.log('Your Answer Incorrect ✘ ' + incorrect);
 
-var question5 = prompt('900+233-500*12/4=?')
-console.log ('Question 5: ' + '900' + '+' + '233' + '-' + '500' + '*' + '12' + '/' + '4' + '=' + (900+233-500*12/4) + ' Your Answer is: ' + question5)
+var question4 = prompt('20+30*50/10=?');
+console.log ('Question 4: ' + '20' + '+' + '30' + '*' + '50' + '/' + '10' + '=' + (20+30*50/10) + ' Your Answer is: ' + question4 )
+
+if (question4 <= 170 && question4 >= 170 )
+    console.log('Your Answer Correct ✓ ' + correct);
+else 
+    console.log('Your Answer Incorrect ✘ ' + incorrect);
+
+var question5 = prompt('90+23-50*12/4=?')
+console.log ('Question 5: ' + '90' + '+' + '23' + '-' + '50' + '*' + '12' + '/' + '4' + '=' + (90+23-50*12/4) + ' Your Answer is: ' + question5)
+
+if (question1 <= -37 && question1 >= -37 )
+    console.log('Your Answer Correct ✓ ' + correct);
+else 
+    console.log('Your Answer Incorrect ✘ ' + incorrect);
 
 var end = 'Open the console to see answers !'
 alert(end)
